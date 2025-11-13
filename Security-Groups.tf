ff331a78a1ea9eeb2f28c88a3de9b5aa78fbf504
SYED AYAZUDDIN <syedayazuddin@SYEDs-MacBook-Air.local>
2025-11-13 15:30:03 +0530

    Teraaform-EC2/ScurityGroup-Creation

---
+++ Security-Groups.tf
@@ -0,0 +1,23 @@
+
+resource "aws_security_group" "Shabu-Security" {
+    name = "Shabu-Security"
+    description = "allow-all with out any restriciton"
+
+ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+tags = {
+    name = "allow-all"
+    }
+}
