9cf9078e84f536221cb3cbb60e06cbea8d77d468
shahabsb94 <shahabsb94@gmail.com>
2025-12-23 11:02:23 +0530

    docker

--- D-EC2/docker.sh
+++ D-EC2/docker.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
-set -euxo pipefail
 
-# Expand disk (Amazon Linux 2023)
-growpart /dev/nvme0n1 4 || true
-lvextend -l +50%FREE /dev/RootVG/rootVol || true
-lvextend -l +50%FREE /dev/RootVG/varVol || true
-xfs_growfs / || true
-xfs_growfs /var || true
+growpart /dev/nvme0n1 4
+lvextend -l +50%FREE /dev/RootVG/rootVol
+lvextend -l +50%FREE /dev/RootVG/varVol
+xfs_growfs /
+xfs_growfs /var
 
-# Install Docker
 dnf -y install dnf-plugins-core
 dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
-dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-systemctl enable docker
+dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 systemctl start docker
-
+systemctl enable docker
 usermod -aG docker ec2-user
-
-# Install telnet (correct way)
-dnf install -y telnet
