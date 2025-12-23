ba0dc5bddffd5e9d0e3d3771b6eaf9b1b3614a78
shahabsb94 <shahabsb94@gmail.com>
2025-12-23 11:22:30 +0530

    docker

--- D-EC2/docker.sh
+++ D-EC2/docker.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
+set -euxo pipefail
 
-growpart /dev/nvme0n1 4
-lvextend -l +50%FREE /dev/RootVG/rootVol
-lvextend -l +50%FREE /dev/RootVG/varVol
-xfs_growfs /
-xfs_growfs /var
+dnf update -y
 
-dnf -y install dnf-plugins-core
+dnf install -y dnf-plugins-core
 dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
-dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-systemctl start docker
+
+dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 systemctl enable docker
+systemctl start docker
+
 usermod -aG docker ec2-user
 
 echo "Docker installed successfully"
-echo "please logout and login again"
